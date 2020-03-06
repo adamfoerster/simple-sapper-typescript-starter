cb575e3f2891e5e07204d75fd1b18f0d6b2644fd
Adam Foerster <adam.foerster@baritecnologia.com.br>
2020-03-06 15:13:50 -0300

    basic

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,10 @@
+<script lang="typescript">
+import {text} from './helper';
+</script>
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
-
+<p>{text}</p>
 <p>This is the 'about' page. There's not much here.</p>
